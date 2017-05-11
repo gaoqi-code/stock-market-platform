2803b1bdf16b2fbd0562f92f0082190b8c436f24
zhangsongwei <zhangsongwei@99114.com>
2017-05-11 11:58:21 +0800

    生产代码提交

---
+++ src/main/java/com/hiveview/common/Constants.java
@@ -0,0 +1,17 @@
+package com.hiveview.common;
+
+
+
+public class Constants {
+	
+	/**
+	 * 上传文件总目录
+	 */
+	public static String UPLOAD = "upload";
+
+	/**
+	 * apk上传目录
+	 */
+	public static String APK="tvapk";
+
+}
